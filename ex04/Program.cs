1e17b5f55ac6d5f88c82cd6087a20a2de36395a7
PEDRO LUCAS ARAUJO LIMA <pedro.lalima@senacsp.edu.br>
2025-01-14 16:37:29 -0300

    exercicio 04

---
+++ ex04/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Qual é o seu nome?");
+string nome = Console.ReadLine();
+
+Console.WriteLine("Qual é o seu sobrenome?");
+string sobrenome = Console.ReadLine();
+
+Console.WriteLine("Qual é o seu ultimo nome?");
+string ultnome = Console.ReadLine();
+
+Console.WriteLine($"Seja bem-vindo(a) {nome} {sobrenome} {ultnome}");
--- ex04/obj/Debug/net8.0/ex04.AssemblyInfo.cs
+++ ex04/obj/Debug/net8.0/ex04.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("ex02")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("ex04")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1cb52f64fcbe053dcc29dba5119f3adb260a96e4")]
-[assembly: System.Reflection.AssemblyProductAttribute("ex02")]
-[assembly: System.Reflection.AssemblyTitleAttribute("ex02")]
+[assembly: System.Reflection.AssemblyProductAttribute("ex04")]
+[assembly: System.Reflection.AssemblyTitleAttribute("ex04")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
