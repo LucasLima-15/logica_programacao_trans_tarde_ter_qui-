229dd433583abeb20175fb9df229be2447d2af75
PEDRO LUCAS ARAUJO LIMA <pedro.lalima@senacsp.edu.br>
2025-01-14 16:56:48 -0300

    exercicio 05

---
+++ ex05/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite o primeiro número");
+int N1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o sengundo número");
+int N2 = int.Parse(Console.ReadLine());
+
+Console.Clear();
+
+Console.WriteLine("=======================");
+Console.WriteLine("=== SOMA DE NÚMEROS ===");
+Console.WriteLine("=======================");
+
+Console.Write($"A soma dos valores é: {N1 + N2}");
