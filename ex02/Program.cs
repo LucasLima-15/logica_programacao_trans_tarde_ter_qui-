1cb52f64fcbe053dcc29dba5119f3adb260a96e4
PEDRO LUCAS ARAUJO LIMA <pedro.lalima@senacsp.edu.br>
2025-01-14 15:23:01 -0300

    first commit

---
+++ ex02/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ola mundo! Eu sou Pedro");
+Console.WriteLine("Estou aprendendo Lógica de Programação");
---
+++ ex03/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ola mundo! Eu sou Pedro");
+Console.WriteLine("Estou estudando no Senac Largo treze.");
+
